cbf2d5447e1acb991fa3fe0872fc60824ef06026
David Omar Garmendia Salazar <david@visualsp.com>
2026-02-27 17:55:19 -0600

    removing studio to work on a different repo

--- cypress/e2e/studio-tests/profile_interactions.cy.js
+++
@@ -1,6 +0,0 @@
-describe('Standardized Regression Suite - Studio Tests', () => {
-  it('performs a professional workflow recording', () => {
-    // Replace with the URL used in the repo or your own target
-    cy.visit('/'); 
-  });
-});
